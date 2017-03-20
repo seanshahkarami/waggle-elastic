d99ec07a1b8c419047bf3e7a1530720e72a3b6f2
Sean Shahkarami <sean.shahkarami@gmail.com>
2017-03-20 09:43:55 -0500

    Storing changes before removing.

--- pull-data.py
+++ pull-data.py
@@ -9,7 +9,6 @@ from elasticsearch import Elasticsearch
 
 es = Elasticsearch()
 
-
 def normalize(s):
     return '_'.join(re.findall('\w+', s.lower()))
 
@@ -18,65 +17,72 @@ def normalize_keys(d):
     return dict((normalize(k), v) for k, v in d.items())
 
 
-nodes = [
-    '0000001e0610b9fd',
-    '0000001e0610ba60',
-    '0000001e0610b9eb',
-    '0000001e0610bbe9',
-    '0000001e0610b9e1',
-]
+r = requests.get('http://beehive1.mcs.anl.gov/api/datasets?version=2raw')
 
-for node_id in nodes:
-    r = requests.get('http://beehive1.mcs.anl.gov/nodes/{}'.format(node_id))
-    assert r.status_code == 200
+if r.status_code != 200:
+    raise RuntimeError('could not get dataset list')
 
-    urls = re.findall('(http.*version=2raw)', r.text)
+datasets = r.json()
 
-    for url in urls:
-        r = requests.get(url)
-        assert r.status_code == 200
+for dataset in datasets:
+    dataset['date'] = datetime.strptime(dataset['date'], '%Y-%m-%d')
 
-        for line in map(bytes.decode, r.iter_lines()):
-            row = line.strip().split(';')
+start = datetime(2017, 1, 1)
+end = datetime(2017, 1, 1)
 
-            try:
-                timestamp = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S.%f')
-            except ValueError:
-                timestamp = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S')
+# can we do a query delete?
+# es.delete('data-20170101')
 
-            plugin_name = row[1]
-            plugin_version = row[2]
-            plugin_instance = row[3]
-            datatype = row[4]
-            raw = unhexlify(row[5])
+for dataset in filter(lambda dataset: start <= dataset['date'] <= end, datasets):
+    r = requests.get(dataset['url'])
 
-            if plugin_name != 'coresense' or plugin_version != '3':
-                continue
+    if r.status_code != 200:
+        print('skipping dataset: {}'.format(dataset['url']))
+        continue
 
-            sensors = normalize_keys(decode_frame(raw))
+    print('adding dataset: {}'.format(dataset['url']))
 
-            for k, vs in sensors.items():
-                sensors[k] = normalize_keys(vs)
+    for line in map(bytes.decode, r.iter_lines()):
+        row = line.strip().split(';')
 
-            data = {
-                'sensors': sensors,
-                'node_id': node_id.lower()[-12:],
-                'timestamp': timestamp.strftime('%Y/%m/%d %H:%M:%S'),
-                'raw': b64encode(raw).decode(),
-            }
+        try:
+            timestamp = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S.%f')
+        except ValueError:
+            timestamp = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S')
 
-            try:
-                data['coresense_id'] = sensors['coresense_id']['mac_address'].lower()
-                del data['sensors']['coresense_id']
-            except KeyError:
-                print('invalid packet')
+        plugin_name = row[1]
+        plugin_version = row[2]
+        plugin_instance = row[3]
+        datatype = row[4]
+        raw = unhexlify(row[5])
 
-            try:
-                data['chemsense_id'] = sensors['chemsense_id']['mac_address'].lower()
-                del data['sensors']['chemsense_id']
-            except KeyError:
-                pass
+        if plugin_name != 'coresense' or plugin_version != '3':
+            continue
 
-            es.index(index='sensor-{:04}-{:02}'.format(timestamp.year, timestamp.month),
-                     doc_type='coresense',
-                     body=data)
+        sensors = normalize_keys(decode_frame(raw))
+
+        for k, vs in sensors.items():
+            sensors[k] = normalize_keys(vs)
+
+        data = {
+            'node_id': dataset['node_id'].lower()[-12:],
+            'timestamp': timestamp.strftime('%Y/%m/%d %H:%M:%S'),
+            'raw': b64encode(raw).decode(),
+            'sensors': sensors,
+        }
+
+        try:
+            data['coresense_id'] = sensors['coresense_id']['mac_address'].lower()
+            del data['sensors']['coresense_id']
+        except KeyError:
+            print('invalid packet')
+
+        try:
+            data['chemsense_id'] = sensors['chemsense_id']['mac_address'].lower()
+            del data['sensors']['chemsense_id']
+        except KeyError:
+            pass
+
+        es.index(index='data-{:04}{:02}{:02}'.format(timestamp.year, timestamp.month, timestamp.day),
+                 doc_type='coresense',
+                 body=data)
