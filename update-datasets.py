70a0c5b0a68bc192fb4aee4879438d215d32fa28
Sean Shahkarami <sean.shahkarami@gmail.com>
2017-05-18 14:35:10 -0500

    Sync datasets before pulling.

--- update-datasets.py
+++ update-datasets.py
@@ -45,6 +45,8 @@ if __name__ == '__main__':
     parser.add_argument('-r', '--refresh', action='store_true')
     args = parser.parse_args()
 
+    update_datasets()
+
     es = elasticsearch.Elasticsearch()
 
     q = {'query': {'bool': {'must': []}}}
